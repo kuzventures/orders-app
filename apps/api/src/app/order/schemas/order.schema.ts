7cdd1c40c42e512bc037cee6af58041184b6d97b
kuzventures <valery.k18+dev@gmail.com>
2025-04-13 12:38:57 +0300

    project setup

---
+++ apps/api/src/app/order/schemas/order.schema.ts
@@ -0,0 +1,63 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { OrderStatus } from '@orders-app/types'; // adjust the path to match your libs
+
+export type OrderDocument = Order & Document;
+
+@Schema({ timestamps: true })
+export class Order {
+  @Prop({ required: true })
+  userId: string;
+
+  @Prop({
+    required: true,
+    type: [
+      {
+        title: { type: String },
+        price: { type: Number },
+        quantity: { type: Number },
+      },
+    ],
+  })
+  products: {
+    title: string;
+    price: number;
+    quantity: number;
+  }[];
+
+  @Prop({ required: true })
+  totalAmount: number;
+
+  @Prop({ required: true, enum: Object.values(OrderStatus) })
+  status: OrderStatus;
+
+  @Prop({
+    required: true,
+    type: {
+      street: String,
+      city: String,
+      postalCode: String,
+      country: String,
+    },
+  })
+  deliveryAddress: {
+    street: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+
+  @Prop({
+    required: true,
+    type: {
+      lat: Number,
+      lng: Number,
+    },
+  })
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
